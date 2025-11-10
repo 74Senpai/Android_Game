f9c9610c2ce14c6cd32d7a8c54cbd4339f7cab59
tribeti <117581928+tribeti@users.noreply.github.com>
2025-11-10 23:49:05 +0700

    keo tha vat the hung

--- app/src/main/java/com/example/gameproject/MainActivity3.java
+++ app/src/main/java/com/example/gameproject/MainActivity3.java
@@ -1,18 +1,7 @@
 package com.example.gameproject;
 
-import android.animation.ObjectAnimator;
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.graphics.Path;
-import android.text.Layout;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -21,7 +10,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,41 +19,12 @@ public class MainActivity3 extends AppCompatActivity {
         RelativeLayout layout = findViewById(R.id.layout_main);
 
         ThaVatTheRoi thaVatTheRoi = new ThaVatTheRoi(layout, this);
+        VatTheHung vatTheHung = new VatTheHung(layout,this);
 
         thaVatTheRoi.BatDauTha();
-
-        TextView lbl_beHung = new TextView(this);
-        lbl_beHung.setText("Be hung");
-        lbl_beHung.setBackgroundColor(Color.RED);
-        lbl_beHung.setTextSize(30);
-        lbl_beHung.setX(60);
-        lbl_beHung.setY(1300f);
-        lbl_beHung.setOnTouchListener(new View.OnTouchListener() {
-            float dX, dY;
-
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        // Lưu vị trí ban đầu khi chạm
-                        dX = view.getX() - event.getRawX();
-                        return true;
-
-                    case MotionEvent.ACTION_MOVE:
-                        // Cập nhật vị trí khi kéo
-                        view.animate()
-                                .x(event.getRawX() + dX)
-                                .setDuration(0)
-                                .start();
-                        return true;
-
-                    default:
-                        return false;
-                }
-            }
-        });
-
-        layout.addView(lbl_beHung);
+        vatTheHung.BatDau();
+        vatTheHung.KeoTha();
+        vatTheHung.themVaoView();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
